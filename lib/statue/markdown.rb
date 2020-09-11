77e9a39b7829eb294dbe1ad660de3c7eaf43680e
Tom Dalling <tom@tomdalling.com>
2020-09-11 21:52:00 +1000

    Switch to Kramdown for markdown
    Redcarpet has issues I can't get around.

--- lib/statue/markdown.rb
+++ lib/statue/markdown.rb
@@ -5,25 +5,15 @@ module Statue
     extend self
 
     def to_html(markdown)
-      redcarpet.render(markdown)
+      opts = {
+        input: 'GFM',
+        hard_wrap: false,
+        auto_ids: false,
+        smart_quotes: ["apos", "apos", "quot", "quot"], # TODO: turn this on
+      }
+      Kramdown::Document.new(markdown, opts).to_html
     end
 
     private
-
-      def redcarpet
-        @redcarpet ||= Redcarpet::Markdown.new(
-          Renderer,
-          fenced_code_blocks: true,
-          autolink: true,
-          strikethrough: true,
-          tables: true,
-        )
-      end
-
-      class Renderer < Redcarpet::Render::HTML
-        include Rouge::Plugins::Redcarpet
-        # TODO: turn this on after getting to parity with old impl
-        # include Redcarpet::Render::SmartyPants
-      end
   end
 end
